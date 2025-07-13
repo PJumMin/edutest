1f4bd3b7d9e247f660e42dbb71817856bfabdb2d
JM <lsaoe157@naver.com>
2025-07-13 20:00:23 +0900

    테스트 시작

---
+++ src/main/java/com/example/eduapp/EduappApplication.java
@@ -0,0 +1,13 @@
+package com.example.eduapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eduapp/EduappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eduapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
